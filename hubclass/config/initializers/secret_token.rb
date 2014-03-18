68eb01fa3bf769e821ce30c3d53dc70579778ed5
gongming <gongmingqm10@foxmail.com>
2014-03-19 00:47:12 +0800

    [gm] first project setup of hubclass

---
+++ hubclass/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hubclass::Application.config.secret_key_base = '21ab25f0014be513549fa6b7c0fbbd6053274d54aeae4d1af804a01c41b6f7d0bcf638046825aaedf54437a93e99d11d1d678393ca79151cf757a9e7ddb87b71'
---
+++ hubclass/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hubclass::Application.config.session_store :cookie_store, key: '_hubclass_session'
